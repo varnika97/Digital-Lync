dadaacc647e7933549c0c329e8c9e155bc615c59
varnika97 <vannuvarnika8@gmail.com>
2025-06-10 00:30:48 +0530

    prgm1

--- PART-II/fs2508App/src/App.jsx
+++ PART-II/fs2508App/src/App.jsx
@@ -5,9 +5,10 @@ function App() {
   
 
   return (
-    <>
-       <h1>Welcome to ReactJS Development Practice</h1>
-    </>
+    <div className='p-5 border border-success'>
+       <h1>Welcome to ReactJS App Development base Practice</h1>
+       <button className= 'btn btn-danger'>Click ME!!</button>
+    </div>
   )
 }
 
