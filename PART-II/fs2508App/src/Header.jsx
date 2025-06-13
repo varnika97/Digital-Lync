378598c5b068096491e5546d84cf04cfdf36be52
varnika97 <vannuvarnika8@gmail.com>
2025-06-13 23:28:48 +0530

    web

---
+++ PART-II/fs2508App/src/Header.jsx
@@ -0,0 +1,12 @@
+
+ function Header(){
+            return <div class="header">
+            <ul>
+                <li>Courses</li>
+                <li>Services</li>
+                <li>Products</li>
+                <li>Contact</li>
+                </ul>
+            </div>
+        }
+export default Header
